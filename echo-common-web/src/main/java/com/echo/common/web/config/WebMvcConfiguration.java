3a3156e5613fa26a8a8606dead57de10807bf2e3
Echo <1038396263@qq.com>
2023-04-17 02:16:18 +0800

    初始

--- echo-common-web/src/main/java/com/echo/common/web/config/WebMvcConfiguration.java
+++ echo-common-web/src/main/java/com/echo/common/web/config/WebMvcConfiguration.java
@@ -59,24 +59,21 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Bean
     public CorsFilter corsFilter() {
-        //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
-        //1) 允许的域,不要写*，否则cookie就无法使用了
-        config.addAllowedOriginPattern("*");
-        //2) 是否发送Cookie信息
         config.setAllowCredentials(true);
-        //3) 允许的请求方式GET POST等
-        config.addAllowedMethod("*");
-        // 4）允许的头信息
+        // 设置访问源地址
+        config.addAllowedOriginPattern("*");
+        // 设置访问源请求头
         config.addAllowedHeader("*");
-        config.setMaxAge(3600L);
-
-        //2.添加映射路径，我们拦截一切请求
-        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
-        configSource.registerCorsConfiguration("/**", config);
-
-        //3.返回新的CorsFilter.
-        return new CorsFilter(configSource);
+        // 设置访问源请求方法
+        config.addAllowedMethod("*");
+        // 有效期 1800秒
+        config.setMaxAge(1800L);
+        // 添加映射路径，拦截一切请求
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        // 返回新的CorsFilter
+        return new CorsFilter(source);
     }
 
 }
